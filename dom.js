fd79f5149c02ac34646d02fedfd25ed2a7dc1c5d
Shashwat Verma <shashwatv18@gmail.com>
2023-12-03 17:18:43 +0530

    dom uploaded

---
+++ dom.js
@@ -0,0 +1,7 @@
+var header = document.getElementById('header-title');
+header.style.borderBottom = 'solid 3px #000';
+
+
+var items = document.getElementsByClassName('title');
+items[0].style.font = 'italic arial,serif';
+items[0].style.color = 'green';
